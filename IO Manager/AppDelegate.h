312cf8bce8b06ac138bcc5b816c795f09b897f40
Matias Seibert <matias@tempest.local>
2011-11-06 12:09:33 -0500

    Initial Commit

---
+++ IO Manager/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  IO Manager
+//
+//  Created by Matias Seibert on 11/6/11.
+//  Copyright (c) 2011 Penn State. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ IO Manager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IO Manager
+//
+//  Created by Matias Seibert on 11/6/11.
+//  Copyright (c) 2011 Penn State. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
